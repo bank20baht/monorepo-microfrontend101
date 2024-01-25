0be61df05c952c9aea0cbdbf6bdb2de150636eae
Nattapong Promthong <banker.z412@gmail.com>
2024-01-25 16:43:36 +0700

    feat: add react backgroud ui in turborepo and use craco and tailwindcss

---
+++ apps/background-ui/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-red-200 flex flex-col justify-center items-center min-h-screen">
+      <div className="text-8xl">background ui</div>
+      <div className="text-red-600">red</div>
+    </div>
+  );
+}
+
+export default App;
