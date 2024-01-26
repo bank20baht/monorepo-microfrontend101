3375ae3ceb961d4def8ba630d54b5f419fcc5db4
Nattapong Promthong <banker.z412@gmail.com>
2024-01-27 01:31:59 +0700

    feat: set up micro frontend by vitejs and simple how to use, yarn build, yarn serve at root dir

--- apps/background-ui/src/App.tsx
+++ apps/background-ui/src/App.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-
 function App() {
   return (
-    <div className="bg-red-200 flex flex-col justify-center items-center min-h-screen">
-      <div className="text-8xl">background ui</div>
-      <div className="text-red-600">red</div>
+    <div className="bg-green-200 flex flex-col justify-center items-center min-h-screen">
+      <div className="text-8xl">Background ui area</div>
     </div>
   );
 }
--- apps/notifications-ui/craco.config.ts
+++
@@ -1,7 +0,0 @@
-import { CracoConfig } from "craco__craco";
-
-const config: CracoConfig = {
-  reactScriptsVersion: "react-scripts",
-};
-
-export default config;
