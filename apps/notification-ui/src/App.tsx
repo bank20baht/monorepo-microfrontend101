a10d2a7bc147f7618578747a22db868a12a5b051
Nattapong Promthong <banker.z412@gmail.com>
2024-01-28 03:17:08 +0700

    feat: add layer for remote component

--- apps/notification-ui/src/App.tsx
+++ apps/notification-ui/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="bg-red-200 flex flex-col justify-center items-center min-h-screen">
+    <div className="bg-red-200 flex flex-col justify-center items-center">
       <div className="text-8xl font-semibold text-gray-500">
         Notification-ui area
       </div>
